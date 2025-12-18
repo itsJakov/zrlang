62e5a27f5139849c6a238808f94326fd4c13e85d
Jakov <23292936+itsJakov@users.noreply.github.com>
2025-12-18 19:21:11 +0100

    compiler: string offset stuff...

--- compiler/compiler/unit_context.py
+++ compiler/compiler/unit_context.py
@@ -1,18 +1,21 @@
 # Compilation Unit Context
 class UnitContext:
-    _strings: dict[str, str] = {} # string -> symbol
+    _strings: dict[str, int] = {} # string -> offset
     string_data: str = ""
     _offset: int = 0
 
-    def string_sym(self, string: str) -> str:
+    def string_offset(self, string: str) -> int:
         existing = self._strings.get(string)
         if existing is not None:
             return existing
 
-        sym = f"$strings+{self._offset}"
-        self._strings[string] = sym
+        offset = self._offset
 
         self.string_data += string + "\\0"
-        self._offset += len(string) + 1 # +1 for the null terminator
+        self._offset += len(string) + 1  # +1 for the null terminator
 
-        return sym
+        return offset
+
+    def string_sym(self, string: str) -> str:
+        offset = self.string_offset(string)
+        return f"$strings+{offset}"
