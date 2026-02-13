75b144ce97457c01a02c01afc8892eef9bb325df
Jakov <23292936+itsJakov@users.noreply.github.com>
2026-02-14 00:46:19 +0100

    stdlib: Add Dictionary test

--- test/rt-playground/main.c
+++ test/rt-playground/main.c
@@ -6,7 +6,6 @@
 
 extern Class User;
 extern Class School;
-extern Class Array;
 
 void testHashing() {
     Instance* user0 = zre_alloc(&User);
@@ -36,7 +35,41 @@ void testHashing() {
     zre_release(user0);
 }
 
+void testDictionary() {
+    extern Class Dictionary;
+
+    Instance* user0 = zre_alloc(&User);
+    zre_call(user0, "init");
+    zre_field_set(user0, "username", (uint64_t)zstr("jakovgz"));
+
+    Instance* user0a = zre_alloc(&User);
+    zre_call(user0a, "init");
+    zre_field_set(user0a, "username", (uint64_t)zstr("jakovgz"));
+
+    Instance* user1 = zre_alloc(&User);
+    zre_call(user1, "init");
+    zre_field_set(user1, "username", (uint64_t)zstr("itsjakov"));
+
+    Instance* usernameToUser = zre_alloc(&Dictionary); // Dictionary<String, User>
+    zre_call(usernameToUser, "init");
+    zre_call(usernameToUser, "set", zre_field_get(user0, "username"), user0);
+    zre_call(usernameToUser, "set", zre_field_get(user1, "username"), user1);
+
+    Instance* retrievedUser0 = zre_call(usernameToUser, "get", zre_field_get(user0, "username"));
+    Instance* retrievedUser1 = zre_call(usernameToUser, "get", zre_field_get(user1, "username"));
+    assert(retrievedUser0 == user0);
+    assert(retrievedUser1 == user1);
+
+    zre_call(usernameToUser, "set", zstr("jakovgz"), user0a);
+    Instance* retrievedUser0a = zre_call(usernameToUser, "get", zstr("jakovgz"));
+    assert(retrievedUser0a == user0a);
+
+    retrievedUser0 = zre_call(usernameToUser, "get", zre_field_get(user0, "username"));
+    assert(retrievedUser0 == user0a);
+}
+
 int main(void) {
     testHashing();
+    testDictionary();
     return 0;
 }
